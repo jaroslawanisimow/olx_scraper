d1d2ffbcf782c560fba2cc5cd1e43ce4a10022d4
Yaro <yaroslavanisimow@gmail.com>
2022-07-12 22:29:35 +0200

    initial commit

---
+++ data.ts
@@ -0,0 +1,308 @@
+export default [
+    {
+        "name": "Fiat 126p jedyny taki. Niski przebieg(REZERWACJA)",
+        "price": 6500,
+        "location": "Gdów ",
+        "link": "https://www.olx.pl/d/oferta/fiat-126p-jedyny-taki-niski-przebiegrezerwacja-CID5-IDPYsoC.html"
+    },
+    {
+        "name": "Fiat Tipo 2017r Salon Polska",
+        "price": 41000,
+        "location": "Wępiły ",
+        "link": "https://www.olx.pl/d/oferta/fiat-tipo-2017r-salon-polska-CID5-IDQ0HOA.html"
+    },
+    {
+        "name": "Fiat Seicento 900",
+        "price": 2000,
+        "location": "Nowy Dwór Mazowiecki ",
+        "link": "https://www.olx.pl/d/oferta/fiat-seicento-900-CID5-IDPZQg0.html"
+    },
+    {
+        "name": "Fiat Stilo 2004r, 1,9 JTD disel",
+        "price": 4700,
+        "location": "Małachowo ",
+        "link": "https://www.olx.pl/d/oferta/fiat-stilo-2004r-1-9-jtd-disel-CID5-IDKkDbC.html"
+    },
+    {
+        "name": "fiat 126p elegant",
+        "price": 1500,
+        "location": "Warszawa, Ursus ",
+        "link": "https://www.olx.pl/d/oferta/fiat-126p-elegant-CID5-IDQ0OMM.html"
+    },
+    {
+        "name": "Fiat Seicento 900",
+        "price": 1700,
+        "location": "Koneck ",
+        "link": "https://www.olx.pl/d/oferta/fiat-seicento-900-CID5-IDQ0OyJ.html"
+    },
+    {
+        "name": "FIAT DUCATO, klimatyzacja, 2.2 diesel",
+        "price": 25900,
+        "location": "Kraków, Mistrzejowice ",
+        "link": "https://www.olx.pl/d/oferta/fiat-ducato-klimatyzacja-2-2-diesel-CID5-IDPOjMS.html"
+    },
+    {
+        "name": "Fiat 126p, maluch",
+        "price": 9900,
+        "location": "Skalno ",
+        "link": "https://www.olx.pl/d/oferta/fiat-126p-maluch-CID5-IDQ0O8N.html"
+    },
+    {
+        "name": "Fiat Tipo 2018 rok 9800km!!!",
+        "price": 47999,
+        "location": "Rakoniewice ",
+        "link": "https://www.olx.pl/d/oferta/fiat-tipo-2018-rok-9800km-CID5-IDQ0Km3.html"
+    },
+    {
+        "name": "Fiat Bravo Sport 1.9 JTD 150 koni 2007 rok NOWY ROZRZAD",
+        "price": 15500,
+        "location": "Kalisz ",
+        "link": "https://www.olx.pl/d/oferta/fiat-bravo-sport-1-9-jtd-150-koni-2007-rok-nowy-rozrzad-CID5-IDPyZ5Z.html"
+    },
+    {
+        "name": "Fiat Croma 2008 1.9 JTD 150 km 16V",
+        "price": 12800,
+        "location": "Glinno ",
+        "link": "https://www.olx.pl/d/oferta/fiat-croma-2008-1-9-jtd-150-km-16v-CID5-IDPTXwU.html"
+    },
+    {
+        "name": "Fiat Doblo ll 1.9 Multijet",
+        "price": 12800,
+        "location": "Ropa ",
+        "link": "https://www.olx.pl/d/oferta/fiat-doblo-ll-1-9-multijet-CID5-IDPOje6.html"
+    },
+    {
+        "name": "Fiat Doblo 2009r 1.4 77KM Wspomaganie Hak Import Niemcy Opłacony",
+        "price": 15900,
+        "location": "Olkusz ",
+        "link": "https://www.otomoto.pl/oferta/fiat-doblo-2009r-1-4-77km-wspomaganie-hak-import-niemcy-oplacony-ID6EGlqr.html"
+    },
+    {
+        "name": "Fiat seicento 900 benzyna // ekonomiczny // miejski // zamiana",
+        "price": 1990,
+        "location": "Bielsko",
+        "link": "https://www.olx.pl/d/oferta/fiat-seicento-900-benzyna-ekonomiczny-miejski-zamiana-CID5-IDPXyhh.html"
+    },
+    {
+        "name": "Fiat 500L 1.4 T-Jet 120 KM, LIVING, climatronic, elektryka, serwis ASO.",
+        "price": 40750,
+        "location": "Kluczbork ",
+        "link": "https://www.otomoto.pl/oferta/fiat-500-1-4-t-jet-120-km-living-climatronic-elektryka-serwis-aso-ID6ELTuO.html"
+    },
+    {
+        "name": "Fiat Grande Punto",
+        "price": 10500,
+        "location": "Nowy Tomyśl ",
+        "link": "https://www.olx.pl/d/oferta/fiat-grande-punto-CID5-IDPY7sl.html"
+    },
+    {
+        "name": "Fiat Grande Punto 1.4 LPG",
+        "price": 5300,
+        "location": "Ełk ",
+        "link": "https://www.olx.pl/d/oferta/fiat-grande-punto-1-4-lpg-CID5-IDPHjLn.html"
+    },
+    {
+        "name": "Fiat Croma 1.9 jtd, 150KM",
+        "price": 14900,
+        "location": "Brynica ",
+        "link": "https://www.olx.pl/d/oferta/fiat-croma-1-9-jtd-150km-CID5-IDQ0NEP.html"
+    },
+    {
+        "name": "Fiat Panda van 1.3 Multijet",
+        "price": 2200,
+        "location": "Mikołajew ",
+        "link": "https://www.olx.pl/d/oferta/fiat-panda-van-1-3-multijet-CID5-IDPp0f2.html"
+    },
+    {
+        "name": "Fiat Punto Kabrio * Unikat * Włoch - stan kolekcjonerski * Bertone",
+        "price": 19900,
+        "location": "Tarnów ",
+        "link": "https://www.otomoto.pl/oferta/fiat-punto-kabrio-unikat-wloch-stan-kolekcjonerski-bertone-ID6EMTYU.html"
+    },
+    {
+        "name": "Fiat Bravo 2007 1,9 multijet 150KM . Salon polska. Pierwszy właściciel",
+        "price": 13300,
+        "location": "Poznań, Winogrady ",
+        "link": "https://www.olx.pl/d/oferta/fiat-bravo-2007-1-9-multijet-150km-salon-polska-pierwszy-wlasciciel-CID5-IDPEJVd.html"
+    },
+    {
+        "name": "Fiat Panda 1.1 benzyna gaz",
+        "price": 4000,
+        "location": "Strzegowo ",
+        "link": "https://www.olx.pl/d/oferta/fiat-panda-1-1-benzyna-gaz-CID5-IDQ0Njs.html"
+    },
+    {
+        "name": "Fiat Croma FIAT CROMA 2.4 DIESEL 200 Km Panorama.",
+        "price": 28900,
+        "location": "Zabrze ",
+        "link": "https://www.otomoto.pl/oferta/fiat-croma-fiat-croma-2-4-diesel-200-km-panorama-ID6ELfej.html"
+    },
+    {
+        "name": "Fiat Seicento 1.1",
+        "price": 2600,
+        "location": "Kalisz ",
+        "link": "https://www.olx.pl/d/oferta/fiat-seicento-1-1-CID5-IDQ0Ngl.html"
+    },
+    {
+        "name": "Fiat Panda 90KM Benzyna 4x4 CD Wspomaganie Blokada mostu ELD",
+        "price": 29800,
+        "location": "Świętoszówka ",
+        "link": "https://www.otomoto.pl/oferta/fiat-panda-90km-benzyna-4x4-cd-wspomaganie-blokada-mostu-eld-ID6EFhyC.html"
+    },
+    {
+        "name": "Fiat Bravo Polski Salon! 1 Właściciel Niski przebieg !",
+        "price": 18900,
+        "location": "Witoldów ",
+        "link": "https://www.olx.pl/d/oferta/fiat-bravo-polski-salon-1-wlasciciel-niski-przebieg-CID5-IDPQhaB.html"
+    },
+    {
+        "name": "Fiat Seicento Young 900 39KM 2000r z Polskiego Salonu Niski Przebieg",
+        "price": 1900,
+        "location": "Bukowno ",
+        "link": "https://www.olx.pl/d/oferta/fiat-seicento-young-900-39km-2000r-z-polskiego-salonu-niski-przebieg-CID5-IDQ06Nu.html"
+    },
+    {
+        "name": "Fiat Stilo 1.4 2004r. 6 biegów",
+        "price": 4500,
+        "location": "Grębków ",
+        "link": "https://www.olx.pl/d/oferta/fiat-stilo-1-4-2004r-6-biegow-CID5-IDPWZBA.html"
+    },
+    {
+        "name": "Fiat 1,2 Punto 2003r sprawny",
+        "price": 4500,
+        "location": "Opole ",
+        "link": "https://www.olx.pl/d/oferta/fiat-1-2-punto-2003r-sprawny-CID5-IDQ0Oor.html"
+    },
+    {
+        "name": "Fiat 126p na sprzedaż",
+        "price": 18000,
+        "location": "Wolin ",
+        "link": "https://www.olx.pl/d/oferta/fiat-126p-na-sprzedaz-CID5-IDQ0N52.html"
+    },
+    {
+        "name": "Fiat Panda 1.1 Gaz ** Zadbana ** Bez rdzy ** Długie opłaty **",
+        "price": 5900,
+        "location": "Zduńska Wola ",
+        "link": "https://www.olx.pl/d/oferta/fiat-panda-1-1-gaz-zadbana-bez-rdzy-dlugie-oplaty-CID5-IDPG9x0.html"
+    },
+    {
+        "name": "Fiat Panda 1.1 8v 54 KM ** Śliczna ** Elektryka ** C. Zamek ** SRS **",
+        "price": 5350,
+        "location": "Zduńska Wola ",
+        "link": "https://www.olx.pl/d/oferta/fiat-panda-1-1-8v-54-km-sliczna-elektryka-c-zamek-srs-CID5-IDPO3bY.html"
+    },
+    {
+        "name": "Fiat Freemont 2013 rok 218000km",
+        "price": 55000,
+        "location": "Kraków, Mistrzejowice ",
+        "link": "https://www.otomoto.pl/oferta/fiat-freemont-2013-rok-218000km-ID6ELgFD.html"
+    },
+    {
+        "name": "Fiat Panda",
+        "price": 6500,
+        "location": "Krotoszyn ",
+        "link": "https://www.otomoto.pl/oferta/fiat-panda-ID6EJWZy.html"
+    },
+    {
+        "name": "Samochód Fiat Ducato 2,3l diesel",
+        "price": 24000,
+        "location": "Luzino ",
+        "link": "https://www.olx.pl/d/oferta/samochod-fiat-ducato-2-3l-diesel-CID5-IDQ0N9u.html"
+    },
+    {
+        "name": "Fiat Bravo Niezawodny silnik",
+        "price": 12600,
+        "location": "Komorniki ",
+        "link": "https://www.otomoto.pl/oferta/fiat-bravo-niezawodny-silnik-ID6ELGS8.html"
+    },
+    {
+        "name": "Fiat Grande Punto 1.4 95 km klima 2 strefowa bardzo ładny stan",
+        "price": 9200,
+        "location": "Małe Kozy ",
+        "link": "https://www.olx.pl/d/oferta/fiat-grande-punto-1-4-95-km-klima-2-strefowa-bardzo-ladny-stan-CID5-IDPKE9P.html"
+    },
+    {
+        "name": "Fiat Ulysse Fiat Ulysse Gaz",
+        "price": 4900,
+        "location": "Będzin, Warpie ",
+        "link": "https://www.otomoto.pl/oferta/fiat-ulysse-fiat-ulysse-gaz-ID6ELgEv.html"
+    },
+    {
+        "name": "Fiat Bravo 1,4 benzyna klima oryginał mały przebieg bezwypadek",
+        "price": 22999,
+        "location": "Żarki ",
+        "link": "https://www.otomoto.pl/oferta/fiat-bravo-1-4-benzyna-klima-oryginal-maly-przebieg-bezwypadek-ID6EJWYD.html"
+    },
+    {
+        "name": "Fiat punto 1,2. 2003 rok",
+        "price": 4200,
+        "location": "Zator ",
+        "link": "https://www.olx.pl/d/oferta/fiat-punto-1-2-2003-rok-CID5-IDPU6eY.html"
+    },
+    {
+        "name": "Sprzedam fiat seicento, w pełni sprawny , serwisowany",
+        "price": 1500,
+        "location": "Kraków, Bieńczyce ",
+        "link": "https://www.olx.pl/d/oferta/sprzedam-fiat-seicento-w-pelni-sprawny-serwisowany-CID5-IDPIyOr.html"
+    },
+    {
+        "name": "Sprzedam Fiata 125p taxi zamienię na Warszawa Nysa",
+        "price": 11500,
+        "location": "Olbrachtów ",
+        "link": "https://www.olx.pl/d/oferta/sprzedam-fiata-125p-taxi-zamienie-na-warszawa-nysa-CID5-IDPWWFa.html"
+    },
+    {
+        "name": "Fiat Qubo * 2010 Rok* 1.3 Diesel * Super Stan* Zamiana* Raty*",
+        "price": 17900,
+        "location": "Lisów ",
+        "link": "https://www.olx.pl/d/oferta/fiat-qubo-2010-rok-1-3-diesel-super-stan-zamiana-raty-CID5-IDMQxqt.html"
+    },
+    {
+        "name": "Fiat Punto Fiat Punto SPORTING 1.4 Ładny Stan",
+        "price": 4890,
+        "location": "Wrocław, Psie Pole ",
+        "link": "https://www.otomoto.pl/oferta/fiat-punto-fiat-punto-sporting-1-4-ladny-stan-ID6EJWXL.html"
+    },
+    {
+        "name": "Fiat Panda bezyna i gaz",
+        "price": 5400,
+        "location": "Nieporęt ",
+        "link": "https://www.otomoto.pl/oferta/fiat-panda-bezyna-i-gaz-ID6EJWX6.html"
+    },
+    {
+        "name": "Fiat Tipo Tipo 1.6 16V E-TorQ z polskiego salonu. Automat. Serwisowany w ASO",
+        "price": 49969,
+        "location": "Warszawa, Ochota ",
+        "link": "https://www.otomoto.pl/oferta/fiat-tipo-tipo-1-6-16v-e-torq-z-polskiego-salonu-automat-serwisowany-w-aso-ID6ELGMH.html"
+    },
+    {
+        "name": "Fiat Panda 1.2 z gazem",
+        "price": 11500,
+        "location": "Gdańsk, Olszynka ",
+        "link": "https://www.olx.pl/d/oferta/fiat-panda-1-2-z-gazem-CID5-IDQ0LV2.html"
+    },
+    {
+        "name": "Na sprzedaż Fiat Panda 2003",
+        "price": 2200,
+        "location": "Morąg ",
+        "link": "https://www.olx.pl/d/oferta/na-sprzedaz-fiat-panda-2003-CID5-IDQ0LSe.html"
+    },
+    {
+        "name": "Fiat Panda 1,1 Salon Pl 82 tys przebieg!!!",
+        "price": 6400,
+        "location": "Złoty Stok ",
+        "link": "https://www.olx.pl/d/oferta/fiat-panda-1-1-salon-pl-82-tys-przebieg-CID5-IDQ0LL8.html"
+    },
+    {
+        "name": "Fiat 500 1.2 8V 69KM I Właściciel Bezwypadkowy Krajowy FV23%",
+        "price": 52900,
+        "location": "Zabrze ",
+        "link": "https://www.otomoto.pl/oferta/fiat-500-1-2-8v-69km-i-wlasciciel-bezwypadkowy-krajowy-fv23-ID6EFPPf.html"
+    },
+    {
+        "name": "Fiat 126p czerwona strzałka",
+        "price": 15000,
+        "location": "Warszawa, Targówek ",
+        "link": "https://www.olx.pl/d/oferta/fiat-126p-czerwona-strzalka-CID5-IDQ0LJf.html"
+    }
+]
